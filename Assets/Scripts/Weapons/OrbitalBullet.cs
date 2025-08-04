e4cb9e9c7c52bcf74e14c5163da948c9a398eefb
Adam vanLangenberg <adam.vanlangenberg@gmail.com>
2025-08-04 21:37:22 +1000

    buuuuuuuuns

---
+++ Assets/Scripts/Weapons/OrbitalBullet.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+
+public class OrbitalBullet : MonoBehaviour
+{
+	private float damage = 1f;
+	private float speed = 12f;
+    private float range = 15f;
+    private float angle;
+    private Transform center;
+
+    public void Initialise(Transform centerPos, float damageVal, float speedVal, float rangeVal, float angleVal)
+    {
+        damage = damageVal;
+        speed = speedVal;
+        range = rangeVal;
+        center = centerPos;
+        angle = angleVal;
+
+        // Set initial position
+        float rad = angle * Mathf.Deg2Rad;
+        Vector3 offset = new Vector3(Mathf.Cos(rad), Mathf.Sin(rad), 0f) * range;
+        transform.position = center.position + offset;
+    }
+
+    private void FixedUpdate()
+    {
+        if (center == null) return;
+
+        // Update angle
+        angle += speed * Time.fixedDeltaTime;
+        angle %= 360f;
+
+        // Calculate new position
+        float rad = angle * Mathf.Deg2Rad;
+        Vector3 offset = new Vector3(Mathf.Cos(rad), Mathf.Sin(rad), 0f) * range;
+        transform.position = center.position + offset;
+
+        //if (time > duration)
+        // {
+        //     Destroy(gameObject);
+        // }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Enemy"))
+        {
+            Enemy enemy = collider.GetComponent<Enemy>();
+            if (enemy != null)
+            {
+                Destroy(gameObject);
+                enemy.TakeDamage(damage);
+            }
+            AudioController.Instance.PlaySound(AudioController.Instance.directionalWeaponHit);
+        }
+    }
+}
---
+++ Assets/Scripts/Weapons/OrbitalWeapon.cs
@@ -0,0 +1,74 @@
+// using System.Runtime.CompilerServices;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class OrbitalWeapon : SpecialWeaponBehaviour
+{
+    [Header("Prefabs")]
+    [SerializeField] private GameObject orbiterPrefab;
+    
+    [SerializeField] private float damage;
+    [SerializeField] private float speed; //degrees per second
+    [SerializeField] private float range;
+    [SerializeField] private int quantity;
+
+    private Transform orbitPoint;
+    private List<OrbitalBullet> bulletList = new List<OrbitalBullet>();
+    private SpecialWeaponBehaviour bulletScript;
+    private bool active = false;
+
+    private void Start()
+    {
+        orbitPoint = transform.Find("Bun");
+        if (orbitPoint == null)
+        {
+            Debug.LogError("OrbitPoint not found as child of " + gameObject.name);
+        }
+    }
+
+    private void Activate()
+    {
+        if (orbitPoint == null)
+        {
+            orbitPoint = transform.parent.Find("Bun");
+            if (orbitPoint == null)
+            {
+                Debug.LogError("OrbitPoint not found on player.");
+                return;
+            }
+        }
+        for (int i = 0; i < quantity; i++)
+        {
+            float angle = (360f / quantity) * i;
+
+            GameObject bulletGO = Instantiate(orbiterPrefab);
+            OrbitalBullet bullet = bulletGO.GetComponent<OrbitalBullet>();
+            bullet.Initialise(orbitPoint, damage, speed, range, angle);
+            bulletList.Add(bullet);
+        }
+    }
+
+    private void Deactivate()
+    {
+        foreach (var bullet in bulletList)
+        {
+            if (bullet != null)
+                Destroy(bullet.gameObject);
+        }
+        bulletList.Clear();
+    }
+
+    public override void ToggleActivate()
+    {
+        if (active)
+        {
+            Deactivate();
+            active = false;
+        }
+        else
+        {
+            Activate();
+            active = true;
+        }
+    }
+}
